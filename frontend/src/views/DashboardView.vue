1407b7de1749d5665ec01147ef9b2aa1df2d3be7
devopMarkz <marcosdev2002@gmail.com>
2025-07-05 21:55:59 -0300

    Criando botao de navegacao para tela de analise de grafico

--- frontend/src/views/DashboardView.vue
+++ frontend/src/views/DashboardView.vue
@@ -387,6 +387,16 @@
           </svg>
           Contas Recorrentes
         </button>
+        <button class="acao-btn" @click="irParaGraficoFinanceiro">
+          <svg width="16" height="16" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+            <line x1="4" y1="20" x2="4" y2="10" stroke="currentColor" stroke-width="2"/>
+            <line x1="10" y1="20" x2="10" y2="6" stroke="currentColor" stroke-width="2"/>
+            <line x1="16" y1="20" x2="16" y2="14" stroke="currentColor" stroke-width="2"/>
+            <line x1="22" y1="20" x2="22" y2="2" stroke="currentColor" stroke-width="2"/>
+          </svg>
+         Gráfico Financeiro
+        </button>
+
         <button class="acao-btn disabled">
           <svg width="16" height="16" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
             <path d="M21 12C21 16.9706 16.9706 21 12 21C7.02944 21 3 16.9706 3 12C3 7.02944 7.02944 3 12 3C16.9706 3 21 7.02944 21 12Z" stroke="currentColor" stroke-width="2"/>
@@ -772,6 +782,10 @@ export default {
 
     irParaContasRecorrentes(){
       this.$router.push('/contas-recorrentes');
+    },
+
+    irParaGraficoFinanceiro(){
+      this.$router.push('/grafico-analise')
     }
   },
   
